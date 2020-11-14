0e155fcc952dad3c10cf0248b48ce1130c6089c1
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-11-14 14:36:32 +0000

    Copied contents of theme.js from mapbox draw API source code to all modification of some part of this whilst maintaining the default parameters of the rest of the API styling. Line color altered from blue to green to match the Survey Lines interface panel and make it clear to the use that the object is a line and not a polygon.

--- assets/js/mapboxCustom.js
+++ assets/js/mapboxCustom.js
@@ -53,7 +53,250 @@ $("document").ready(function () {
         line_string: true, //Draw Line
         trash: true, //Delete
       },
+    //Mapbox draw styles copied from mapbox-gl-draw/src/lib/theme.js and modified for custom styling of lines.
+      styles: [
+        {
+          id: "gl-draw-polygon-fill-inactive",
+          type: "fill",
+          filter: [
+            "all",
+            ["==", "active", "false"],
+            ["==", "$type", "Polygon"],
+            ["!=", "mode", "static"],
+          ],
+          paint: {
+            "fill-color": "#3bb2d0",
+            "fill-outline-color": "#3bb2d0",
+            "fill-opacity": 0.1,
+          },
+        },
+        {
+          id: "gl-draw-polygon-fill-active",
+          type: "fill",
+          filter: ["all", ["==", "active", "true"], ["==", "$type", "Polygon"]],
+          paint: {
+            "fill-color": "#fbb03b",
+            "fill-outline-color": "#fbb03b",
+            "fill-opacity": 0.1,
+          },
+        },
+        {
+          id: "gl-draw-polygon-midpoint",
+          type: "circle",
+          filter: ["all", ["==", "$type", "Point"], ["==", "meta", "midpoint"]],
+          paint: {
+            "circle-radius": 3,
+            "circle-color": "#fbb03b",
+          },
+        },
+        {
+          id: "gl-draw-polygon-stroke-inactive",
+          type: "line",
+          filter: [
+            "all",
+            ["==", "active", "false"],
+            ["==", "$type", "Polygon"],
+            ["!=", "mode", "static"],
+          ],
+          layout: {
+            "line-cap": "round",
+            "line-join": "round",
+          },
+          paint: {
+            "line-color": "#3bb2d0",
+            "line-width": 2,
+          },
+        },
+        {
+          id: "gl-draw-polygon-stroke-active",
+          type: "line",
+          filter: ["all", ["==", "active", "true"], ["==", "$type", "Polygon"]],
+          layout: {
+            "line-cap": "round",
+            "line-join": "round",
+          },
+          paint: {
+            "line-color": "#fbb03b",
+            "line-dasharray": [0.2, 2],
+            "line-width": 2,
+          },
+        },
+        {
+          id: "gl-draw-line-inactive",
+          type: "line",
+          filter: [
+            "all",
+            ["==", "active", "false"],
+            ["==", "$type", "LineString"],
+            ["!=", "mode", "static"],
+          ],
+          layout: {
+            "line-cap": "round",
+            "line-join": "round",
+          },
+          paint: {
+            //Modified the line-color property to Green to match Survey Lines interface pane.
+            "line-color": "#56b714",
+            "line-width": 2,
+          },
+        },
+        {
+          id: "gl-draw-line-active",
+          type: "line",
+          filter: [
+            "all",
+            ["==", "$type", "LineString"],
+            ["==", "active", "true"],
+          ],
+          layout: {
+            "line-cap": "round",
+            "line-join": "round",
+          },
+          paint: {
+            "line-color": "#fbb03b",
+            "line-dasharray": [0.2, 2],
+            "line-width": 2,
+          },
+        },
+        {
+          id: "gl-draw-polygon-and-line-vertex-stroke-inactive",
+          type: "circle",
+          filter: [
+            "all",
+            ["==", "meta", "vertex"],
+            ["==", "$type", "Point"],
+            ["!=", "mode", "static"],
+          ],
+          paint: {
+            "circle-radius": 5,
+            "circle-color": "#fff",
+          },
+        },
+        {
+          id: "gl-draw-polygon-and-line-vertex-inactive",
+          type: "circle",
+          filter: [
+            "all",
+            ["==", "meta", "vertex"],
+            ["==", "$type", "Point"],
+            ["!=", "mode", "static"],
+          ],
+          paint: {
+            "circle-radius": 3,
+            "circle-color": "#fbb03b",
+          },
+        },
+        {
+          id: "gl-draw-point-point-stroke-inactive",
+          type: "circle",
+          filter: [
+            "all",
+            ["==", "active", "false"],
+            ["==", "$type", "Point"],
+            ["==", "meta", "feature"],
+            ["!=", "mode", "static"],
+          ],
+          paint: {
+            "circle-radius": 5,
+            "circle-opacity": 1,
+            "circle-color": "#fff",
+          },
+        },
+        {
+          id: "gl-draw-point-inactive",
+          type: "circle",
+          filter: [
+            "all",
+            ["==", "active", "false"],
+            ["==", "$type", "Point"],
+            ["==", "meta", "feature"],
+            ["!=", "mode", "static"],
+          ],
+          paint: {
+            "circle-radius": 3,
+            "circle-color": "#3bb2d0",
+          },
+        },
+        {
+          id: "gl-draw-point-stroke-active",
+          type: "circle",
+          filter: [
+            "all",
+            ["==", "$type", "Point"],
+            ["==", "active", "true"],
+            ["!=", "meta", "midpoint"],
+          ],
+          paint: {
+            "circle-radius": 7,
+            "circle-color": "#fff",
+          },
+        },
+        {
+          id: "gl-draw-point-active",
+          type: "circle",
+          filter: [
+            "all",
+            ["==", "$type", "Point"],
+            ["!=", "meta", "midpoint"],
+            ["==", "active", "true"],
+          ],
+          paint: {
+            "circle-radius": 5,
+            "circle-color": "#fbb03b",
+          },
+        },
+        {
+          id: "gl-draw-polygon-fill-static",
+          type: "fill",
+          filter: ["all", ["==", "mode", "static"], ["==", "$type", "Polygon"]],
+          paint: {
+            "fill-color": "#404040",
+            "fill-outline-color": "#404040",
+            "fill-opacity": 0.1,
+          },
+        },
+        {
+          id: "gl-draw-polygon-stroke-static",
+          type: "line",
+          filter: ["all", ["==", "mode", "static"], ["==", "$type", "Polygon"]],
+          layout: {
+            "line-cap": "round",
+            "line-join": "round",
+          },
+          paint: {
+            "line-color": "#404040",
+            "line-width": 2,
+          },
+        },
+        {
+          id: "gl-draw-line-static",
+          type: "line",
+          filter: [
+            "all",
+            ["==", "mode", "static"],
+            ["==", "$type", "LineString"],
+          ],
+          layout: {
+            "line-cap": "round",
+            "line-join": "round",
+          },
+          paint: {
+            "line-color": "#404040",
+            "line-width": 2,
+          },
+        },
+        {
+          id: "gl-draw-point-static",
+          type: "circle",
+          filter: ["all", ["==", "mode", "static"], ["==", "$type", "Point"]],
+          paint: {
+            "circle-radius": 5,
+            "circle-color": "#404040",
+          },
+        },
+      ],
     });
+
     //Place mapbox draw tools in #toolbar div.
     document.getElementById("toolbar").appendChild(draw.onAdd(map));
 
@@ -123,17 +366,17 @@ $("document").ready(function () {
           let areaKm = (area / 1000).toFixed(2);
           if (feature.geometry.type === "Polygon") {
             let polygonCoords = feature.geometry.coordinates[0];
-            //Use polygonCoords and convert to a lineString in turf and then use turf to calculate the length of the linestring in kilometers to 2d.p. 
+            //Use polygonCoords and convert to a lineString in turf and then use turf to calculate the length of the linestring in kilometers to 2d.p.
             let perimeterLength = turf
               .length(turf.lineString(polygonCoords))
               .toFixed(2);
-            //Call the function to write these statistics into the Statistics window. 
+            //Call the function to write these statistics into the Statistics window.
             updateBoundaryStats(areaKm, perimeterLength);
             return polygonCoords;
           }
         }
       }
-      //Write the calculated polygon statistics into the relevant html elements. 
+      //Write the calculated polygon statistics into the relevant html elements.
       function updateBoundaryStats(areaKm, perimeterLength) {
         $("#boundaryStatData").html(
           `<p id="boundaryArea" class="bgGlassSmokeIncOpacity"><strong>Boundary Area (km) : </strong>` +
@@ -147,7 +390,7 @@ $("document").ready(function () {
       }
       //Boundary Variable containing the coordinate array of the polygon.
       let boundary = polygonTarget();
-      //Write the boundary vertices into the boundary table. 
+      //Write the boundary vertices into the boundary table.
       function writeBoundaryToTable() {
         $("#boundaryCoords>#boundaryTable>tbody>tr").remove();
         if (typeof boundary !== "undefined") {
@@ -225,7 +468,7 @@ $("document").ready(function () {
           if (feature.geometry.type === "LineString") {
             //Get coordinates of each lines vertices
             let lineCoords = feature.geometry.coordinates;
-            //Push coordinate of all vertices for an individual line into an array. 
+            //Push coordinate of all vertices for an individual line into an array.
             lineFeaturesArray.push(lineCoords);
           }
         }
@@ -235,7 +478,6 @@ $("document").ready(function () {
       //Line Variable containing the coordinate array of the lineString
       let lines = lineTarget();
       //Gets line string coorinates, matches then to a line ID and writes them to a table for the user.
-
       function writeLineToTable() {
         //Delete existing lines from Survey Lines table.
         $("#lineCoords>#lineTable>tbody>tr").remove();
@@ -243,16 +485,16 @@ $("document").ready(function () {
         $("#lineStatsTable>tbody>tr").remove();
         //Update number of lines in statistics.
         $("#lineCount").html(" ");
-        //Update total length of all lines in statistics. 
+        //Update total length of all lines in statistics.
         $("#lineDistance").html(" ");
-        //Show the warning to promt users to re-calulate duration statistics. 
+        //Show the warning to promt users to re-calulate duration statistics.
         $("#recalcWarning").show();
-        //As long as the line type is not undefined perform this function to write lines to Survey Lines table and calculate each line's length. 
+        //As long as the line type is not undefined perform this function to write lines to Survey Lines table and calculate each line's length.
         if (typeof lines !== "undefined") {
           let lineLengthsArray = [];
           for (let i = 0; i < lines.length; i++) {
             let lineLengthArray = [];
-            //Use turf to calculate each lines length. 
+            //Use turf to calculate each lines length.
             let lineLength = turf.length(turf.lineString(lines[i])).toFixed(2);
             let lineId = i + 1;
             let line = lines[i];
@@ -302,7 +544,6 @@ $("document").ready(function () {
         }
       }
       writeLineToTable();
-
       //Function to simulate an enter key down event, used to trigger the functions in UTMproject.js to convert the LAT LONG boundary to Easting and Northing.
       //Adapted from cloakedninjas response within a Stack Overflow query June 2013.
       function simShift() {
