641bb408dde3ce7ec74031d90493d839104b5636
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-11-14 11:52:16 +0000

    Added comments/notes to the interfaceCollapse.js script.

--- assets/js/interfaceCollapse.js
+++ assets/js/interfaceCollapse.js
@@ -1,12 +1,15 @@
 $("document").ready(function(){
+    //When the Boundary header is clicked it will hide the other left interface pane and toggle the inteface pane which the boundary header is associated with. 
     $("#boundaryHead").click(function(){
         $("#leftInterfaceB").hide();
         $("#leftInterface").toggle();
     });
+    //When the Survey Lines header is clicked it will hide the other left interface pane and toggle the inteface pane which the Survey Lines header is associated with.
     $("#linesHead").click(function(){
         $("#leftInterface").hide();
          $("#leftInterfaceB").toggle();
     });
+    //When the Statistics header is clicked it will toggle the inteface pane which the Statistics header is associated with.
     $("#statsHead").click(function(){
          $("#rightInterface").toggle();
     });
